46c05ca554756356842361fb5ff9dcd6169b44f7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-15 22:39:49 -0300

    feat(to-do-aplication): adiciona anotacao de auditoria

--- src/main/java/dev/joaorooliveira/to_do/ToDoApplication.java
+++ src/main/java/dev/joaorooliveira/to_do/ToDoApplication.java
@@ -2,8 +2,10 @@ package dev.joaorooliveira.to_do;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ToDoApplication {
 
 	public static void main(String[] args) {
