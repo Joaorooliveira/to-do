685e2de6b644b274da2b7931bdb4a9f606938e9e
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-15 22:40:04 -0300

    feat: adiciona classe tarefa

--- src/main/java/dev/joaorooliveira/to_do/domain/Tarefa.java
+++ src/main/java/dev/joaorooliveira/to_do/domain/Tarefa.java
@@ -1,5 +1,49 @@
 package dev.joaorooliveira.to_do.domain;
 
+import dev.joaorooliveira.to_do.enums.TipoPrioridade;
+import dev.joaorooliveira.to_do.enums.TipoStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
+@Table(name = "tarefa")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private TipoStatus status = TipoStatus.PENDENTE;
+
+    @Enumerated(EnumType.STRING)
+    private TipoPrioridade prioridade;
+
+    @Future
+    private LocalDate prazo;
+
+    @CreatedDate
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    private LocalDateTime atualizadoEm;
 }
