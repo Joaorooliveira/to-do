5264ef4ae339ab8dab2482f00f1a385c100672d2
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-15 22:13:32 -0300

    first commit

---
+++ src/test/java/dev/joaorooliveira/to_do/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaorooliveira.to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
