3b917d3c2cd84773ca1a97a831e821ae9d6b6cca
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-17 21:29:35 -0300

    feat(tarefa-service): adiciona metodo para buscar tarefa por id

--- src/main/java/dev/joaorooliveira/to_do/service/TarefaService.java
+++ src/main/java/dev/joaorooliveira/to_do/service/TarefaService.java
@@ -1,7 +1,10 @@
 package dev.joaorooliveira.to_do.service;
 
+import dev.joaorooliveira.to_do.dto.TarefaRequestDTO;
+import dev.joaorooliveira.to_do.dto.TarefaResponseDTO;
 import dev.joaorooliveira.to_do.repository.TarefaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TarefaService {
@@ -11,4 +14,21 @@ public class TarefaService {
     public TarefaService(TarefaRepository tarefaRepository) {
         this.tarefaRepository = tarefaRepository;
     }
+
+    @Transactional
+    public TarefaResponseDTO salvarTarefa(TarefaRequestDTO dto) {
+        var tarefa = dto.toEntity();
+        tarefaRepository.save(tarefa);
+        return TarefaResponseDTO.fromEntity(tarefa);
+    }
+
+    public TarefaResponseDTO buscarTarefaPorId(Long id) {
+        var tarefa = tarefaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tarefa não encontrada"));
+        return TarefaResponseDTO.fromEntity(tarefa);
+    }
+
+
+
+
 }
