3b0aaddfa0191625f91d0da9422d6b3999043b23
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-19 20:12:13 -0300

    feat(tarefa-atualizar-dto): adiciona metodo para preencher entidade

--- src/main/java/dev/joaorooliveira/to_do/dto/TarefaAtualizarDTO.java
+++ src/main/java/dev/joaorooliveira/to_do/dto/TarefaAtualizarDTO.java
@@ -1,5 +1,6 @@
 package dev.joaorooliveira.to_do.dto;
 
+import dev.joaorooliveira.to_do.domain.Tarefa;
 import dev.joaorooliveira.to_do.enums.TipoPrioridade;
 import dev.joaorooliveira.to_do.enums.TipoStatus;
 
@@ -12,4 +13,28 @@ public record TarefaAtualizarDTO(
         TipoPrioridade prioridade,
         LocalDate prazo
 ) {
+
+    public Tarefa toEntity(TarefaAtualizarDTO dto) {
+        Tarefa tarefa = new Tarefa();
+        preencher(tarefa);
+        return tarefa;
+    }
+
+    public void preencher(Tarefa tarefa) {
+        if (titulo != null) {
+            tarefa.setTitulo(titulo);
+        }
+        if (descricao != null) {
+            tarefa.setDescricao(descricao);
+        }
+        if (status != null) {
+            tarefa.setStatus(status);
+        }
+        if (prioridade != null) {
+            tarefa.setPrioridade(prioridade);
+        }
+        if (prazo != null) {
+            tarefa.setPrazo(prazo);
+        }
+    }
 }
