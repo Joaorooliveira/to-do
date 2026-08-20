24c54c84f279e96a6b22e53cdcf553317bf1b3b4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-19 21:23:24 -0300

    feat(tarefa-repository): adiciona metodo para buscar projection

--- src/main/java/dev/joaorooliveira/to_do/repository/TarefaRepository.java
+++ src/main/java/dev/joaorooliveira/to_do/repository/TarefaRepository.java
@@ -1,8 +1,26 @@
 package dev.joaorooliveira.to_do.repository;
 
 import dev.joaorooliveira.to_do.domain.Tarefa;
+import dev.joaorooliveira.to_do.projection.PrioridadeAltaProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long>, JpaSpecificationExecutor<Tarefa> {
+
+    @Query(
+            value = """
+            SELECT
+                titulo,
+                descricao,
+                status
+            FROM tarefa
+            WHERE prioridade = 'ALTA'
+            """,
+            nativeQuery = true
+    )
+    List<PrioridadeAltaProjection> prioridadeAlta();
+
 }
